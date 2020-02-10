823e1e81b1ee2c52c81a528aff24232c269ae779
Antoine Meausoone <antoine.meausoone@gmail.com>
2020-02-10 15:29:32 +0100

    ci(github): add tf docs

--- technical-module-a/main.tf
+++ technical-module-a/main.tf
@@ -1,3 +1,20 @@
+/*
+ *
+ * # technical-module-a
+ *
+ * A terraform module to create a technical-module-a
+ *
+ *
+ * ## Usage example
+ *
+ * ```hcl
+ * module "technical-module-a" {
+ *   source               = "git@github.com:adeo/technical-module-a"
+ * }
+ * ```
+ *
+ */
+
 provider "local" {
   version = "1.4.0"
 }
--- technical-module-b/main.tf
+++ technical-module-b/main.tf
@@ -1,3 +1,19 @@
+/*
+ *
+ * # technical-module-b
+ *
+ * A terraform module to create a technical-module-b
+ *
+ * ## Usage example
+ *
+ * ```hcl
+ * module "technical-module-b" {
+ *   source               = "git@github.com:adeo/technical-module-b"
+ * }
+ * ```
+ *
+ */
+
 provider "local" {
   version = "1.4.0"
 }
